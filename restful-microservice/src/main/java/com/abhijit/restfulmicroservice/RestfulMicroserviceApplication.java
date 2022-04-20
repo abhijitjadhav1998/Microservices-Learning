d2af141c6d959dfb8f31084d4568cd8154a39a60
Abhijit Jadhav <abhijit.jadhav1998@gmail.com>
2022-04-20 13:06:00 +0530

    added new project

---
+++ restful-microservice/src/main/java/com/abhijit/restfulmicroservice/RestfulMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.abhijit.restfulmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulMicroserviceApplication.class, args);
+	}
+
+}
---
+++ restful-microservice/src/test/java/com/abhijit/restfulmicroservice/RestfulMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhijit.restfulmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
