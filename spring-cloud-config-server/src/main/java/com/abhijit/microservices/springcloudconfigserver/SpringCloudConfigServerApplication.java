892aec67b55adf6e392afc7f7e2080f1d8b7b40d
Abhijit Jadhav <abhijit.jadhav1998@gmail.com>
2022-04-21 14:38:29 +0530

    added some microservice code

--- spring-cloud-config-server/src/main/java/com/abhijit/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
+++ spring-cloud-config-server/src/main/java/com/abhijit/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -2,7 +2,8 @@ package com.abhijit.microservices.springcloudconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.config.server.EnableConfigServer;
+@EnableConfigServer
 @SpringBootApplication
 public class SpringCloudConfigServerApplication {
 
