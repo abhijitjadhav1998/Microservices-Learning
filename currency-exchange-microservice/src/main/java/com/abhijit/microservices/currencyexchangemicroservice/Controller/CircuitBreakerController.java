e82cb7ae8c11a244171c81442417350e7f5a7e89
Abhijit Jadhav <abhijit.jadhav1998@gmail.com>
2022-04-23 14:59:14 +0530

    added some microservice code

--- currency-exchange-microservice/src/main/java/com/abhijit/microservices/currencyexchangemicroservice/Controller/CircuitBreakerController.java
+++ currency-exchange-microservice/src/main/java/com/abhijit/microservices/currencyexchangemicroservice/Controller/CircuitBreakerController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
@@ -30,7 +32,17 @@ public class CircuitBreakerController {
 		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
 		return forEntity.getBody();
 	}
+	@GetMapping("/sample-api-ratelimiter")
+	@RateLimiter(name = "default") // in 10 second allow 1000 calls to the sample api
+	public String sampleApiRateLimiter() {
+		return "rate limiter";
+	}
 	
+	@GetMapping("/sample-api-bulkhead")
+	@Bulkhead(name = "default") // in 10 second allow 1000 calls to the sample api
+	public String sampleApiBulkhead() {
+		return "BulK limiter";
+	}
 	
 	private String hardcodedResponse(Exception exception) {
 		return "Microservice http://localhost:8080/some-dummy-url is down";
