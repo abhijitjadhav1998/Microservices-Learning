05b35ab9f5d9ff8f095c2517bb2b578380a90f1c
Abhijit Jadhav <abhijit.jadhav1998@gmail.com>
2022-04-23 13:20:45 +0530

    added some microservice code

--- currency-exchange-microservice/src/main/java/com/abhijit/microservices/currencyexchangemicroservice/Controller/CircuitBreakerController.java
+++ currency-exchange-microservice/src/main/java/com/abhijit/microservices/currencyexchangemicroservice/Controller/CircuitBreakerController.java
@@ -1,15 +1,23 @@
 package com.abhijit.microservices.currencyexchangemicroservice.Controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.retry.annotation.Retry;
+
 @RestController
 public class CircuitBreakerController {
+	
+	private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
+	
 	@GetMapping("/sample-api")
-	//@Retry(name = "")
+	@Retry(name = "default")
 	public String sampleApi() {
+		logger.info("Sample API call recieved");
 		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
 		return forEntity.getBody();
 	}
